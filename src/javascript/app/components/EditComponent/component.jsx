68393180718dfff4bb22ed75f02bc99f5dd008bf
HerrZatacke <zatacke@gmail.com>
2020-06-30 14:41:44 +0200

    styling/backdrop

--- src/javascript/app/components/EditComponent/component.jsx
+++ src/javascript/app/components/EditComponent/component.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 
 const EditComponent = ({ fields, updateFieldValue, saveEdit }) => (
   fields ? (
-    <div className="edit-component">
-      {Object.keys(fields).map((field) => (
-        <input
-          type="text"
-          key={field}
-          name={field}
-          placeholder={field}
-          value={fields[field]}
-          onChange={({ target }) => updateFieldValue(field, target.value)}
-        />
-      ))}
-      <br />
-      <button
-        type="submit"
-        onClick={() => saveEdit()}
-      >
-        Save
-      </button>
-    </div>
+    <>
+      <div className="edit-component__backdrop" />
+      <div className="edit-component">
+        {Object.keys(fields).map((field) => (
+          <input
+            type="text"
+            key={field}
+            name={field}
+            placeholder={field}
+            value={fields[field]}
+            onChange={({ target }) => updateFieldValue(field, target.value)}
+          />
+        ))}
+        <br />
+        <button
+          type="submit"
+          onClick={() => saveEdit()}
+        >
+          Save
+        </button>
+      </div>
+    </>
   ) : null
 );
 
--- src/javascript/app/data/coral-components/components/button.js
+++ src/javascript/app/data/coral-components/components/button.js
@@ -2,7 +2,7 @@ const button = {
   name: 'Button',
   description: 'Just a button',
   id: 'button',
-  src: `<button class="coral-Form-field">
+  src: `<button class="coral-Button coral-Button--secondary">
   {label}
 </button>`,
 };

b2f5fa9ad8c268ae384ed620cf818cadaa407682
HerrZatacke <zatacke@gmail.com>
2020-07-31 13:14:52 +0200

    eslint error

--- src/javascript/app/components/DialogPreview/conditions/component.jsx
+++ src/javascript/app/components/DialogPreview/conditions/component.jsx
@@ -5,7 +5,7 @@ const component = {
     node.classList && node.classList.contains('coral-Form-fieldwrapper')
   ),
   action: (node, key, level, parser) => (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions,jsx-a11y/click-events-have-key-events
     <div
       className="coral-Form-fieldwrapper"
       title={node.dataset.title}

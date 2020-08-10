962ae0cc1a546553e106ec0df627a6605c9661f9
thomas.vogt <thomas.vogt@diva-e.com>
2020-08-10 10:51:00 +0200

    make it runnable again

--- src/javascript/app/components/Adapt/component.jsx
+++ src/javascript/app/components/Adapt/component.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-cycle
 import dom2reactRenderer from '../../../tools/dom2reactRenderer';
 
 const Adapt = ({ children }) => (
--- src/javascript/tools/dom2reactRenderer.js
+++ src/javascript/tools/dom2reactRenderer.js
@@ -1,14 +1,13 @@
 import Dom2react from 'dom-to-react';
 import droptarget from '../app/components/DialogPreview/conditions/droptarget';
 import component from '../app/components/DialogPreview/conditions/component';
-// eslint-disable-next-line import/no-cycle
-import adapt from '../app/components/DialogPreview/conditions/adapt';
+// import adapt from '../app/components/DialogPreview/conditions/adapt';
 
+// todo: readd adapt
 const dom2reactRenderer = () => (
   new Dom2react([
     droptarget,
     component,
-    adapt,
   ])
 );
 

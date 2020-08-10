291780a164e2b6fe0377a5c365ea3ae82d2bf8b9
thomas.vogt <thomas.vogt@diva-e.com>
2020-08-10 11:51:54 +0200

    fixing datepicker

--- src/javascript/app/data/coral-components/components/normal/datepicker.js
+++ src/javascript/app/data/coral-components/components/normal/datepicker.js
@@ -14,15 +14,30 @@ const datepicker = {
     FIELD_DEFINITION_DESCRIPTION,
     FIELD_DEFINITION_REQUIRED,
     {
-      id: 'placeholder',
-      label: 'Placeholder',
+      id: 'type',
+      label: 'Type',
       description: '',
       type: 'String',
-      defaultValue: 'Choose a date',
+      options: [
+        {
+          value: Coral.Datepicker.type.DATE,
+          caption: 'Date',
+        },
+        {
+          value: Coral.Datepicker.type.DATETIME,
+          caption: 'Date Time',
+        },
+        {
+          value: Coral.Datepicker.type.TIME,
+          caption: 'Time',
+        },
+      ],
+      defaultValue: Coral.Datepicker.type.DATETIME,
+      required: true,
     },
     {
-      id: 'valueformat',
-      label: 'Value Format',
+      id: 'placeholder',
+      label: 'Placeholder',
       description: '',
       type: 'String',
       defaultValue: 'Choose a date',
@@ -32,32 +47,31 @@ const datepicker = {
       label: 'Display Format',
       description: '',
       type: 'String',
-      defaultValue: 'Choose a date',
+      defaultValue: 'YYYY-MM-DD',
     },
     {
       id: 'startday',
       label: 'Startday',
       description: '',
       type: 'Long',
-      defaultValue: 'Choose a date',
+      defaultValue: '0',
     },
   ],
-  /* todo: preview output */
   src: `<coral-datepicker
-      placeholder="Choose a date"
-      name="field"
-      value="2015-01-15"
-      valueformat="YYYY-MM-DD"
-      displayformat="YYYY-MM-DD"
+      placeholder={placeholder}
+      name={id}
+      value=""
+      valueformat={displayformat}
+      displayformat={displayformat}
       startday="0">
     </coral-datepicker>`,
   xml: `<{id}
     jcr:primaryType="nt:unstructured"
     sling:resourceType="granite/ui/components/coral/foundation/form/datepicker"
-    displayedFormat="MM-DD-YYYY HH:mm"
+    displayedFormat="{displayformat}"
     fieldLabel="{label}"
     name="./{id}"
-    type="datetime"
+    type="{datetime}"
     typeHint="Date"/>`,
 };
 

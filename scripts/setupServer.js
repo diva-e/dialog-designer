0de27a2e2f03071ae8f354f3b8001270d190f056
Andreas Hahn <andreas.hahn@diva-e.com>
2020-08-18 12:08:36 +0200

    simple basic auth with environment variable

---
+++ scripts/setupServer.js
@@ -0,0 +1,22 @@
+const basicAuth = require('express-basic-auth');
+const queryString = require('query-string');
+
+const setupServer = (app) => {
+
+  const basicAuthUserPass = process.argv.indexOf('BASIC_AUTH_USER_PASS');
+
+  if (basicAuthUserPass) {
+    const users = queryString.parse(basicAuthUserPass);
+
+    const config = {
+      users,
+      unauthorizedResponse: '<h1>Unauthorized [401]</h1>',
+      challenge: true,
+      realm: 'dnd-dialogs',
+    };
+
+    app.use(basicAuth(config));
+  }
+};
+
+module.exports = setupServer;

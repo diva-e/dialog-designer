410f716d328d6f4d25d5b732f67a3bc57bdc2d67
Thomas Vogt <thomas.vogt@diva-e.com>
2020-08-25 08:25:14 +0200

    general progress

---
+++ src/javascript/app/components/ActionPanelContainer/ActionPanel/component.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ActionPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.openPanel = this.openPanel.bind(this);
+  }
+
+  openPanel() {
+    console.log('opened');
+    this.props.isActive = true;
+  }
+
+  render() {
+    const {
+      title,
+      children,
+      isActive,
+      icon,
+    } = this.props;
+    return (
+      <>
+        <button
+          className="action-panel__content-opener"
+          onClick={this.openPanel}
+          type="button"
+        >
+          { icon ? (
+            <coral-icon
+              icon={icon}
+              size="S"
+            />
+          ) : null }
+          <span>{title}</span>
+        </button>
+        { isActive ? (
+          <div className="action-panel">
+            {children}
+          </div>
+        ) : null }
+      </>
+    );
+  }
+}
+
+ActionPanel.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  isActive: PropTypes.bool.isRequired,
+  icon: PropTypes.string,
+};
+
+ActionPanel.defaultProps = {
+  children: null,
+  icon: null,
+};
+
+export default ActionPanel;

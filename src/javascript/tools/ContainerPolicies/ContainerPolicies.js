97255edc488f83bd9b1345cc978bd53437bab47a
Thomas Vogt <thomas.vogt@diva-e.com>
2020-09-01 14:22:07 +0200

    #26 supporting group policies in container config

--- src/javascript/tools/ContainerPolicies/ContainerPolicies.js
+++ src/javascript/tools/ContainerPolicies/ContainerPolicies.js
@@ -2,17 +2,47 @@
 import coralComponents from '../../app/data/coral-components';
 
 const allComponentIds = coralComponents.map(({ id }) => (id));
+const GROUP_POLICY_PREFIX = 'group:';
 
 class ContainerPolicies {
-  static hasPolicyConfig(containerName) {
+  static hasPolicyConfig(policyString) {
     // eslint-disable-next-line no-undef
-    return CONTAINER_POLICY.hasOwnProperty(containerName);
+    return CONTAINER_POLICY.hasOwnProperty(policyString);
   }
 
-  static getPolicy(containerName) {
-    if (this.hasPolicyConfig(containerName)) {
+  static isGroupPolicy(policyString) {
+    return policyString ? policyString.startsWith(GROUP_POLICY_PREFIX) : false;
+  }
+
+  static getPolicy(policyString) {
+    if (this.hasPolicyConfig(policyString)) {
+      console.log('####policyString', policyString);
       // eslint-disable-next-line no-undef
-      return CONTAINER_POLICY[containerName];
+      let policies = CONTAINER_POLICY[policyString];
+
+      const detectedGroupPolicies = policies.filter((pc) => this.isGroupPolicy(pc));
+      console.log('detectedGroupPolicies', detectedGroupPolicies);
+      policies = policies.filter((policy) => !this.isGroupPolicy(policy));
+      console.log('policies after', policies);
+
+      if (detectedGroupPolicies.length > 0) {
+        const compiledGroupPolicy = coralComponents
+          .filter((component) => (
+            detectedGroupPolicies.find((ps) => {
+              const extractedGroupPolicyName = ps.replace(GROUP_POLICY_PREFIX, '').trim();
+              console.log('extractedGroupPolicyName', extractedGroupPolicyName);
+              return component.category === extractedGroupPolicyName;
+            })
+          ))
+          .map(({ id }) => (id));
+
+        console.log('+++++++compiledGroupPolicy', compiledGroupPolicy);
+
+        policies = policies.concat(compiledGroupPolicy);
+      }
+
+      console.log('after after', policies);
+      return policies;
     }
 
     // all components

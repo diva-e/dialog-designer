28e9b05edd56cdecfc4742900312a20540164c0d
Roy Teeuwen <roy.teeuwen@amplexor.com>
2020-10-09 20:57:49 +0200

    Add upload functionality based on the existing download json

--- src/javascript/app/components/Footer/container.js
+++ src/javascript/app/components/Footer/container.js
@@ -10,9 +10,20 @@ const mapDispatchToProps = (dispatch) => ({
     });
   },
   downloadJSON: () => {
+  
     dispatch({
       type: actionNames.STRUCTURE.DOWNLOAD.JSON,
     });
+  },
+  uploadJSON: (event) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(event.target.files[0], 'UTF-8');
+    fileReader.onload = (loadedEvent) => {
+      dispatch({
+        type: actionNames.STRUCTURE.SET,
+        payload: JSON.parse(loadedEvent.target.result),
+      });
+    };
   },
   downloadACGJSON: () => {
     dispatch({

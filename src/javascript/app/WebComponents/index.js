415144c9396743340a9d1bdeba8fd79f27dd55d1
Andrei <mail@andreishilov.dev>
2020-12-10 18:34:07 +0100

    #11 - cleanup WC section

--- src/javascript/app/WebComponents/index.js
+++ src/javascript/app/WebComponents/index.js
@@ -1,9 +1,7 @@
 import initDropTargetWebComponent from './DropTarget';
-// import initComponentWrapper from './ComponentWrapper';
 
 const initWebComponents = (store) => {
   initDropTargetWebComponent(store);
-  // initComponentWrapper(store);
 };
 
 export default initWebComponents;
